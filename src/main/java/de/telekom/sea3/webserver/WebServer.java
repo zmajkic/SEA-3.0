7fdbfef7e71841b42fe40cd16f0a2deb360db337
zmajkic <zeljko.majkic@telekom.de>
2021-06-09 11:32:46 +0200

    webserver spring

---
+++ src/main/java/de/telekom/sea3/webserver/WebServer.java
@@ -0,0 +1,11 @@
+package de.telekom.sea3.webserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServer /* implements SpringBootApplication */ {
+	public static void main(String[] args) {
+		SpringApplication.run(WebServer.class, args);
+	}
+}
