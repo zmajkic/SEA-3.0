19fca74158ac858cfb2640821cca3b9c73ce355c
zmajkic <zeljko.majkic@telekom.de>
2021-06-10 13:07:13 +0200

    json vorname

--- src/main/resources/public/javaScript/myscript.js
+++ src/main/resources/public/javaScript/myscript.js
@@ -6,3 +6,15 @@ para.textContent = "Hello from JavaScript";
 fetch("personen.json")
    .then (irgendwas => irgendwas.json())
    .then(myjson => console.log(myjson.personen[0]));
+
+   
+   
+   
+   
+   
+   var cell =  document.getElementById("id4712");
+   fetch("personen.json")
+   .then (irgendwas => irgendwas.json())
+   .then(myjson => cell.textContent=myjson.personen[0].vorname);
+
+   
